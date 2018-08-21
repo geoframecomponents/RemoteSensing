b5c5a5459fbc4714e5ca4a98dd1b203e1a21a03a
sidereus <francesco.serafin.3@gmail.com>
2018-08-21 08:51:08 -0600

    cha: local path unit test

--- src/test/java/CentroidsGeneratorTest/CentroidTest.java
+++ src/test/java/CentroidsGeneratorTest/CentroidTest.java
@@ -13,12 +13,12 @@ public class CentroidTest {
 		Centroid centroids= new Centroid();
 	
 
-		centroids.inFolder="/home/drugo/eclipse-workspace/CentroidsGenerator/resources/Input/";// /Users/marialaura/Dropbox/dati_NewAge/EsercitazioniIdrologia2017/data/Basilicata/lai";
+		centroids.inFolder="./resources/Input/";// /Users/marialaura/Dropbox/dati_NewAge/EsercitazioniIdrologia2017/data/Basilicata/lai";
 		
 		//	centroids.nameOfOutput = "Centroid";	
 			centroids.referenceSystem = "32632";
 			centroids.inputDataType=".asc";
-			centroids.outputPath ="/home/drugo/eclipse-workspace/CentroidsGenerator/resources/Output/";
+			centroids.outputPath ="./resources/Output/";
 			centroids.nameOfOutput="Centroid";
 			
 			centroids.process();
